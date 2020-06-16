fc4a50697199f65d96a1a2296515c52048bcb4cb
shahar shvarts <shahar.shvarts@gmail.com>
2020-06-16 12:44:22 +0300

    Updates

---
+++ precache-manifest.95b9b5e415ce5cadf60228f6348d8934.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9285c75a332a8d4a040bcf7eaf0d9f9e",
+    "url": "/ITlogic/index.html"
+  },
+  {
+    "revision": "389aafdc050e486ef59b",
+    "url": "/ITlogic/static/css/main.ccce7070.chunk.css"
+  },
+  {
+    "revision": "6e0adae2be64d3636193",
+    "url": "/ITlogic/static/js/2.14ea989e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ITlogic/static/js/2.14ea989e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389aafdc050e486ef59b",
+    "url": "/ITlogic/static/js/main.37a98ad0.chunk.js"
+  },
+  {
+    "revision": "380236eaa31bf0053a6a",
+    "url": "/ITlogic/static/js/runtime-main.a9de930a.js"
+  },
+  {
+    "revision": "a3f80d0c995e007b773b201debdbb720",
+    "url": "/ITlogic/static/media/bg_login.a3f80d0c.jpg"
+  },
+  {
+    "revision": "73a281d945c78e9df6d5d88c3d313c09",
+    "url": "/ITlogic/static/media/phi_logo.73a281d9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df53a7a8871db5ad511895288cac58da.js"
+  "/ITlogic/precache-manifest.95b9b5e415ce5cadf60228f6348d8934.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ITlogic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
